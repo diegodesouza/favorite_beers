2127c1430d23a95965e7f406dcabdfc3663ef372
Helen Hood <helen.c.hood@gmail.com>
2014-11-19 16:34:17 -0500

    Read beer info from CSV

--- app.rb
+++ app.rb
@@ -4,7 +4,16 @@ require 'pry'
 require 'csv'
 
 def read_beers_from(filename)
+  beers = []
 
+  CSV.foreach(filename, headers: true) do |row|
+    beers << {
+      name: row['name'],
+      description: row['description']
+    }
+  end
+
+  beers
 end
 
 get '/beers' do
