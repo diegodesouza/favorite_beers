c2d0bafa912f39148d76f52a1e8cc7bb20ec1cdb
Helen Hood <helen.c.hood@gmail.com>
2014-11-19 17:19:22 -0500

    Retain params when rerender form

--- app.rb
+++ app.rb
@@ -36,12 +36,12 @@ post '/beers' do
   # {"beer"=>{"name"=>"Pumpkinhead", "description"=>"Delicious!"}}
 
   # take the beer params from params hash
-  if !params[:beer][:name].empty? && !params[:beer][:description].empty?
-    name = params[:beer][:name]
-    description = params[:beer][:description]
+  @name = params[:beer][:name]
+  @description = params[:beer][:description]
 
+  if !params[:beer][:name].empty? && !params[:beer][:description].empty? && params[:beer][:description].length >= 4
     # add the beer to the CSV
-    save_beer('beers.csv', name, description)
+    save_beer('beers.csv', @name, @description)
 
     # redirect the user to the index page
     redirect '/beers'
@@ -49,6 +49,7 @@ post '/beers' do
     @error_messages = []
     @error_messages << "You must enter a name." if params[:beer][:name].empty?
     @error_messages << "You must enter a description." if params[:beer][:description].empty?
+    @error_messages << "Description must be 4 characters or longer." if params[:beer][:description].length < 4
     # if params[:beer][:name].empty?
     #   @error_messages << "You must enter a name."
     # end
