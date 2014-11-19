d025fdb9dae9a5f4689fb256c910b2ca3be194bc
Helen Hood <helen.c.hood@gmail.com>
2014-11-19 16:32:15 -0500

    beers index page rendering

--- app.rb
+++ app.rb
@@ -2,3 +2,12 @@ require 'sinatra'
 require 'shotgun'
 require 'pry'
 require 'csv'
+
+def read_beers_from(filename)
+
+end
+
+get '/beers' do
+  @beers = read_beers_from('beers.csv')
+  erb :'beers/index'
+end
